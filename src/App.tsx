ed1ef5483f8a02585791e7eebde59f82de53556e
Agent-Kwabbel <fabestuffken@proton.me>
2025-10-04 11:56:43 +0200

    Aurora background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 
+import AuroraCanvas from "./components/AuroraCanvas.tsx"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <main className="min-h-screen relative">
+        <AuroraCanvas />
+      </main>
     </>
   )
 }
